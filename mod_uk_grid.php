334ae6211d2bc0ac9ee260af32115d31064faabe
Aleksey A. Morozov <AlekVolsk@users.noreply.github.com>
2018-06-14 03:06:43 +0400

    Add files via upload

--- mod_uk_grid.php
+++ mod_uk_grid.php
@@ -1,17 +1,28 @@
 <?php defined('_JEXEC') or die;
+/*
+ * @package     mod_uk_grid
+ * @copyright   Copyright (C) 2018 Aleksey A. Morozov (AlekVolsk). All rights reserved.
+ * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
+ */
 
 $vars = [
-    'moduleclass_sfx', 
-    'tile_class', 'margin_class', 'first_column_class', 'height_match', 'height_match_class', 
-    'grid', 'grid_divider', 'grid_center', 'cols', 'cols_all', 'cols_s', 'cols_m', 'cols_l', 'cols_xl',
+    'grid_class', 'item_class', 'margin_class', 'first_column_class', 'height_match', 'height_match_class', 
+    'grid', 'grid_divider', 'grid_center', 'cols', 'cols_all', 'cols_s', 'cols_m', 'cols_l', 'cols_xl', 'masonry',
     'items'
 ];
+
 foreach ($vars as $var)
+{
     $$var = $params->get($var);
+}
+
+$grid_class = trim($grid_class) ? ' ' . trim($grid_class) : '';
+$item_class = trim($item_class);
 
 $grid_params = [];
 if ($margin_class) $grid_params[] = 'margin_class:.' . $margin_class;
 if ($first_column_class) $first_column_class[] = 'margin_class:.' . $first_column_class;
+if ($masonry) $grid_params[] = 'masonry:true';
 $grid_params = $grid_params ? '="' . implode(';', $grid_params) . '"' : '';
 
 $hm_param = '';
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,15 +1,23 @@
-<?php defined('_JEXEC') or die; ?>
+<?php defined('_JEXEC') or die;
+/*
+ * @package     mod_uk_grid
+ * @copyright   Copyright (C) 2018 Aleksey A. Morozov (AlekVolsk). All rights reserved.
+ * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
+ */
 
-<div class="mod_uk_grid<?php echo $moduleclass_sfx; ?> <?php echo $classes; ?>" data-uk-grid<?php echo $grid_params, $hm_param; ?>>
+?>
+
+<div class="mod_uk_grid<?php echo $grid_class, $classes; ?>" data-uk-grid<?php echo $grid_params, $hm_param; ?>>
     <?php
     foreach ($items as $item)
     {
-        $item_class = trim($tile_class . ' ' . $item->class);
+        $item_add_class = trim($item->class) ? ' ' . trim($item->class) : '';
+        $item_class = $item_class || $item_add_class ? ' class="' . $item_class . $item_add_class . '"' : '';
     ?>
     <div>
-        <div<?php echo ($item_class ? ' class="' . $item_class . '"' : ''); ?>>
-            <?php if ($item->img_front) { ?>
-            <div class="<?php echo $item->al_img; ?>"><img src="<?php echo $item->img_front; ?>" alt="<?php echo $item->title; ?>"></div>
+        <div<?php echo $item_class; ?>>
+            <?php if ($item->img) { ?>
+            <div class="<?php echo $item->al_img; ?>"><img src="<?php echo $item->img; ?>" alt="<?php echo $item->title; ?>"></div>
             <?php } ?>
             
             <?php if ($item->title) { ?>
